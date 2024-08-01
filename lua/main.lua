e15b0a625b2870efb61429390c690856a60e379d
Kalindu De Costa <kalinduk.decosta@gmail.com>
2024-08-01 17:39:20 -0400

    Automatically update when new version is available

--- lua/main.lua
+++ lua/main.lua
@@ -1,1 +1,16 @@
-print("I am the main file!")
+local VERSION_FILE_URL = "https://raw.githubusercontent.com/kalindudc/oh-my-turtles/main//lua/VERSION"
+
+
+local current_version = http.get(VERSION_FILE_URL).readAll():gsub("\n$", "")
+
+while true do
+  print("Checking for updates...")
+  local new_version = http.get(VERSION_FILE_URL).readAll():gsub("\n$", "")
+  if new_version ~= current_version then
+    print("New version available: " .. new_version)
+    print("Updating...")
+    shell.run("update")
+  end
+
+  os.sleep(1)
+end
